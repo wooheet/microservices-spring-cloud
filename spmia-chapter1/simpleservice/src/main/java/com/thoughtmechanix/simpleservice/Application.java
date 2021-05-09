ccbe7fc8a9f41d2a46cb46c22814213b5f314b32
gary <gary@spoonradio.co>
2021-05-10 02:46:15 +0900

    init

--- spmia-chapter1/simpleservice/src/main/java/com/thoughtmechanix/simpleservice/Application.java
+++ spmia-chapter1/simpleservice/src/main/java/com/thoughtmechanix/simpleservice/Application.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@SpringBootApplication
-@RestController
-@RequestMapping(value="hello")
+@SpringBootApplication // 스프링 부트 프레임워크에 이 클래스가 스프링 부트 서비스의 진입점
+@RestController // 스프링 부트에 이 클래스의 코드를 스프링 RestController클래스로 노출하도록 지정
+@RequestMapping(value="hello") // 애플리케이션에서 노출된 url
 public class Application {
 
     public static void main(String[] args) {
